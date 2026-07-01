aa262e5577139014321fa85e3a85f3a6ca403591
Kathy82319 <cath82319@gmail.com>
2026-07-02 06:20:44 +0800

    結束時間改為可以到晚上10點

--- public/group-gathering.js
+++ public/group-gathering.js
@@ -511,7 +511,7 @@ const GatherModule = (() => {
         const allTimes = [
             '12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30',
             '16:00','16:30','17:00','17:30','18:00','18:30','19:00','19:30',
-            '20:00','20:30','21:00'
+            '20:00','20:30','21:00','21:30','22:00'
         ];
         const startTimeEl = document.getElementById('gc-start-time');
         const endTimeEl = document.getElementById('gc-end-time');
